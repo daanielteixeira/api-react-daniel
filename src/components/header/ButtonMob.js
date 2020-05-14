0c4beec399d480615d49cee53b981d355d2662ec
daanielteixeira <57972429+daanielteixeira@users.noreply.github.com>
2020-05-14 15:04:40 -0300

    Update ButtonMob.js

--- src/components/header/ButtonMob.js
+++ src/components/header/ButtonMob.js
@@ -4,15 +4,6 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 export default class ButtonMob extends React.Component {
 
-
-	constructor(){
-		super();
-		this.state = {
-			visNav = 'flex'
-		}
-	}
-
-	
 	
 
 	render(){
